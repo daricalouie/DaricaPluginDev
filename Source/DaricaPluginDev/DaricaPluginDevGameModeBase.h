f3e8bb71bde9421e1d7bd8a653c72f902a8e09e9
Darica Louie <darica.louie@gmail.com>
2020-03-02 12:44:04 -0800

    set up repo with submodules

---
+++ Source/DaricaPluginDev/DaricaPluginDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DaricaPluginDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARICAPLUGINDEV_API ADaricaPluginDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
